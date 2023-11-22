edee1fd6e45a3b6642484935d4072b10b649837e
Max Percy <percy020@umn.edu>
2023-11-21 19:16:44 -0600

    everything but a couple of error messages is done for P4?

--- src/ReadCsv.java
+++ src/ReadCsv.java
@@ -25,36 +25,41 @@ public class ReadCsv
         //list of maps, where each map represents a row in the csv
         List<Map<String, String>> data = read(csvFilePath);
 
+        List<Map<String, String>> modifiedList = data;
 
         while(true)
         {
-
-            
-            List<Map<String, String>> modifiedList = data;
 
             System.out.println("What analysis would you like to run on this file?");
             System.out.println("Current Analysis Options:");
             System.out.println("1) Find Instances: *find all rows for which a specified column has a specified value");
             System.out.println("2) To be continued...");
             System.out.println("Type 'Print' to print the found rows");
+            System.out.println("Type 'Reset' to return to the full dataset");
             System.out.println("Type 'Stop' to end the program");
 
-            String command = myScan.nextLine().toLowerCase();
+            String command = myScan.nextLine();
 
-            if (command.equals("1") || command.equals("1) find instances") || command.equals("find instances")) {
+            if (command.equals("1") || command.equals("1) find instances") || command.equals("find instances") || command.equals("Find Instances")) {
                 String column, keyword;
                 System.out.println("Please choose the column of data that you'd like to filter");
-                column = myScan.nextLine().toLowerCase();
+                column = myScan.nextLine();
                 System.out.println("Choose the keyword/value that you're searching for in that column");
-                keyword = myScan.nextLine().toLowerCase();
+                keyword = myScan.nextLine();
 
                 modifiedList = analyzer.findInstances(modifiedList, column, keyword);
             }
-            else if (command.equals("print")) {
+            else if (command.equals("print") || command.equals("Print"))
+            {
                 printer.printList(modifiedList);
             }
-            else if (command.equals("stop")) {
+            else if (command.equals("stop") || command.equals("Stop"))
+            {
                 return;
+            }
+            else if (command.equals("reset") || command.equals("Reset"))
+            {
+                modifiedList = data;
             }
             else
             {
