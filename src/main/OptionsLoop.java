5f3010b4888aaed270ad5c6ff9a31185f5e00cba
Max Percy <percy020@umn.edu>
2023-12-11 17:22:30 -0600

    option to print text file with entry count

--- src/main/OptionsLoop.java
+++ src/main/OptionsLoop.java
@@ -4,10 +4,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public class OptionsLoop 
-{
-    public void mainLoop(List<Map<String, Object>> data, String csvName) 
-    {
+public class OptionsLoop {
+    public void mainLoop(List<Map<String, Object>> data, String csvName) {
         Analyze analyzer = new Analyze();
         Output output = new Output();
         Scanner myScan = new Scanner(System.in);
@@ -15,56 +13,49 @@ public class OptionsLoop
         // new list of maps will be used if the user modifies anything during analysis
         List<Map<String, Object>> modifiedList = data;
 
-        try 
-        {
-            while (true) 
-            {
+        try {
+            while (true) {
 
                 // options for the user to choose from
-                System.out.println("What would you like to do with the current working dataset? Input a number from the list below.");
+                System.out.println(
+                        "What would you like to do with the current working dataset? Input a number from the list below.");
                 System.out.println("1) See Analysis Choices");
                 System.out.println("2) Output a CSV of Current Dataset");
                 System.out.println("3) Print Current Dataset");
                 System.out.println("4) Reset to Original Dataset");
                 System.out.println("5) End Program/Next File");
+                System.out.println("6) Output Current Set # of Entries");
 
                 String command = myScan.nextLine();
 
                 // call find instance based on user input which will possibly update the list to
                 // only include the searched instances
-                if (command.equals("1") || command.equals("1) find instances") || command.equals("find instances"))
-                {
+                if (command.equals("1") || command.equals("1) find instances") || command.equals("find instances")) {
                     modifiedList = analysisOptions(data);
                 }
                 // print the current working set
-                else if (command.equals("2") || command.equalsIgnoreCase("Output")) 
-                {
+                else if (command.equals("2") || command.equalsIgnoreCase("Output")) {
                     output.outputCsv(modifiedList, csvName);
-                }
-                else if (command.equals("3") || command.equalsIgnoreCase("Print")) 
-                {
+                } else if (command.equals("3") || command.equalsIgnoreCase("Print")) {
                     output.printList(modifiedList);
                 }
                 // change the current working set back to the full set from the csv
-                else if (command.equals("4") || command.equalsIgnoreCase("Reset")) 
-                {
+                else if (command.equals("4") || command.equalsIgnoreCase("Reset")) {
                     modifiedList = data;
                 }
                 // end the program
-                else if (command.equals("5") || command.equalsIgnoreCase("end program")) 
-                {
-                    //myScan.close();
+                else if (command.equals("5") || command.equalsIgnoreCase("end program")) {
+                    // myScan.close();
                     return;
+                } else if (command.equals("6")) {
+                    output.outputEntryCount(modifiedList, csvName);
                 }
                 // error if unrecognized input is given
-                else 
-                {
+                else {
                     System.out.println("*Please choose one of the listed options*");
                 }
             }
-        } 
-        catch (Exception e) 
-        {
+        } catch (Exception e) {
             System.out.println("An error occured when trying to run main loop. Message: " + e.getMessage());
             e.printStackTrace();
         }
@@ -93,8 +84,7 @@ public class OptionsLoop
 
                 // call find instance based on user input which will possibly update the list to
                 // only include the searched instances
-                if (command.equals("1") || command.equalsIgnoreCase("find instances")) 
-                {
+                if (command.equals("1") || command.equalsIgnoreCase("find instances")) {
                     String column, keyword;
                     System.out.println("Please choose the column of data that you'd like to filter");
                     column = myScan.nextLine();
@@ -102,67 +92,49 @@ public class OptionsLoop
                     keyword = myScan.nextLine();
 
                     modifiedList = analyzer.findInstances(modifiedList, column, keyword);
-                }
-                if (command.equals("2") || command.equalsIgnoreCase("less than")) 
-                {
+                } else if (command.equals("2") || command.equalsIgnoreCase("less than")) {
                     String column;
                     double value;
                     System.out.println("Please choose the column of data that you'd like to filter");
                     column = myScan.nextLine();
-                    if (!data.get(0).containsKey(column)) 
-                    {
+                    if (!data.get(0).containsKey(column)) {
                         System.out.println("Invalid column name.");
                         return data;
                     }
 
                     System.out.println("What value is the maximum for the data you'd like to receive?");
-                    try 
-                    {                                        
+                    try {
                         value = Double.parseDouble(myScan.nextLine());
-                    } 
-                    catch (NumberFormatException e) 
-                    {
+                    } catch (NumberFormatException e) {
                         System.out.println("Invalid input for the threshold value. Please enter a numeric value.");
                         return data;
                     }
 
                     modifiedList = analyzer.lessThan(modifiedList, column, value);
-                }
-                if (command.equals("3") || command.equalsIgnoreCase("greater than")) 
-                {
+                } else if (command.equals("3") || command.equalsIgnoreCase("greater than")) {
                     String column;
                     double value;
                     System.out.println("Please choose the column of data that you'd like to filter");
                     column = myScan.nextLine();
-                    if (!data.get(0).containsKey(column)) 
-                    {
+                    if (!data.get(0).containsKey(column)) {
                         System.out.println("Invalid column name.");
                         return data;
                     }
 
                     System.out.println("What value is the minimum for the data you'd like to receive?");
-                    try 
-                    {                                        
+                    try {
                         value = Double.parseDouble(myScan.nextLine());
-                    } 
-                    catch (NumberFormatException e) 
-                    {
+                    } catch (NumberFormatException e) {
                         System.out.println("Invalid input for the threshold value. Please enter a numeric value.");
                         return data;
                     }
 
                     modifiedList = analyzer.greaterThan(modifiedList, column, value);
-                }
-                else if (command.equals("4") || command.equalsIgnoreCase("frequency analysis")) 
-                {
+                } else if (command.equals("4") || command.equalsIgnoreCase("frequency analysis")) {
                     System.out.println("Assume the frequency analysis works");
-                }
-                else if (command.equals("5") || command.equalsIgnoreCase("dependency mapping")) 
-                {
+                } else if (command.equals("5") || command.equalsIgnoreCase("dependency mapping")) {
                     System.out.println("Assume the dependency mapping works");
-                }
-                else if (command.equals("6") || command.equalsIgnoreCase("Return to Main Options")) 
-                {
+                } else if (command.equals("6") || command.equalsIgnoreCase("Return to Main Options")) {
                     return modifiedList;
                 }
                 // error if wrong input
--- src/main/Output.java
+++ src/main/Output.java
@@ -7,37 +7,34 @@ import java.io.IOException;
 import java.io.FileWriter;
 
 public class Output {
-    
-    public Output() {}
 
-    public void printList(List<Map<String, Object>> list)
-    {
-        for(Map<String, Object> row : list)
-        {
+    public Output() {
+    }
+
+    public void printList(List<Map<String, Object>> list) {
+        for (Map<String, Object> row : list) {
             System.out.println(row);
         }
     }
 
-    public void outputCsv(List<Map<String, Object>> list, String csvName)
-    {
+    public void outputCsv(List<Map<String, Object>> list, String csvName) {
         try {
-            csvName = csvName.substring(0, csvName.length()-4);
+            csvName = csvName.substring(0, csvName.length() - 4);
 
             // Creates the new CSV file and sets up the writer for the file
             File newCsv = new File(csvName + "output.csv");
             FileWriter writer = new FileWriter(csvName + "_output.csv");
             String headerString = "";
-            
+
             // Gets all of the column headers and adds them to a string
             for (String header : list.get(0).keySet()) {
                 headerString += header + ",";
             }
 
             // Gets rid of the last comma along with writing the columns into the CSV file
-            headerString = headerString.substring(0, headerString.length()-1);
+            headerString = headerString.substring(0, headerString.length() - 1);
             writer.write(headerString + "\n");
 
-            
             // Gets each row's value and inputs it in the file
             for (Map<String, Object> row : list) {
                 String valueString = "";
@@ -47,17 +44,38 @@ public class Output {
                     valueString += stringValue + ",";
                 }
 
-                valueString = valueString.substring(0, valueString.length()-1);
+                valueString = valueString.substring(0, valueString.length() - 1);
                 writer.write(valueString + "\n");
-                        
+
             }
 
             writer.close();
 
-        // Used for any unexpected IO errors
+            // Used for any unexpected IO errors
         } catch (IOException e) {
             System.out.println("An error while producing new CSV file occured. Message: " + e.getMessage());
             e.printStackTrace();
         }
     }
+
+    public void outputEntryCount(List<Map<String, Object>> list, String csvName) {
+        try {
+            csvName = csvName.substring(0, csvName.length() - 4);
+
+            // Creates the new text file and sets up the writer for the file
+            File newTextFile = new File(csvName + "_num_found.txt");
+            FileWriter writer = new FileWriter(newTextFile);
+
+            // Writes the count of entries to the text file
+            writer.write("Number of entries: " + list.size() + "\n");
+
+            writer.close();
+
+            // Used for any unexpected IO errors
+        } catch (IOException e) {
+            System.out.println("An error while producing entry count file occurred. Message: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
 }
